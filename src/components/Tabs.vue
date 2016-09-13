4a060cde243298ddab45b572f72713878edf7309
scq000 <scq000@hotmail.com>
2016-09-13 15:35:51 +0800

    add css style to Tabs components

--- src/components/Tabs.vue
+++ src/components/Tabs.vue
@@ -1,7 +1,7 @@
 <template>
-<ul>
-  <li v-for="tab of tabs" @click="selectTab(tab)">
-    {{tab.header}}
+<ul class="tabs">
+  <li class="tab-header" v-for="tab of tabs" @click="selectTab(tab)">
+    <a href="#">{{tab.header}}</a>
   </li>
 </ul>
 <slot></slot>
@@ -28,5 +28,44 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="less">
+@height: 50px;
+
+.tabs {
+  width: 100%;
+  height: @height;
+  box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.5);
+  background: #f6f0e2;
+
+  display: flex;
+  position: relative;
+  overflow: hidden;
+
+  li {
+    flex: 1;
+    line-height: @height;
+    overflow: hidden;
+    padding: 0;
+    position: relative;
+  }
+
+  a {
+    color: #f05f30;
+    font-weight: 500;
+    display: block;
+    letter-spacing: 0;
+    outline: none;
+    padding: 0 20px;
+    text-decoration: none;
+    -webkit-transition: all 0.2s ease-in-out;
+    -moz-transition: all 0.2s ease-in-out;
+    transition: all 0.2s ease-in-out;
+
+    &:hover {
+      color: rgba(240,95,48,0.5);
+      cursor: pointer;
+      border-bottom: 1px solid green;
+    }
+  }
+}
 
 </style>
