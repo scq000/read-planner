472e7997f71f82a23d4226f73eb6b37c4ddfd11f
scq000 <scq000@hotmail.com>
2016-09-19 19:30:39 +0800

    add Component BookShelf

---
+++ src/components/BookShelf.vue
@@ -0,0 +1,47 @@
+<template>
+	<div class="bookshelf">
+		<div class="book" v-for="book of books">BookName: {{book.name}}</div>
+	</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      books: [{
+        name: 'nihao',
+        author: '12323',
+      }, {
+        name: '323232',
+        author: '12323',
+      }, {
+        name: '323232',
+        author: '12323',
+      }, {
+        name: '323232',
+        author: '12323',
+      }],
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+@height: 800px;
+.bookshelf {
+  height: @height;
+	width: 100%;
+	display: flex;
+	border: 1px solid green;
+	flex-flow: row wrap;
+	justify-content: flex-start;
+}
+
+.book {
+	width: 180px;
+	height: 300px;
+	border: 1px solid red;
+}
+
+</style>
